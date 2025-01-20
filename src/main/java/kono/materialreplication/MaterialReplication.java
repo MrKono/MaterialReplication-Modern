2a4cb796f7d36bf13ec426dee5f306b80546123b
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-01-21 00:47:42 +0900

    setup minimum main class

---
+++ src/main/java/kono/materialreplication/MaterialReplication.java
@@ -0,0 +1,11 @@
+package kono.materialreplication;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MaterialReplication.MODID)
+public class MaterialReplication {
+    public static final String MODID = "materialreplication";
+
+    public MaterialReplication() {
+    }
+}
