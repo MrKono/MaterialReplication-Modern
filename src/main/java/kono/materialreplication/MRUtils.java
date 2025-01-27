a6ae998062fc7d2a2cc3166a14199c4f16b26437
ko_no <90126004+MrKono@users.noreply.github.com>
2025-01-27 23:15:10 +0900

    Add Config

--- src/main/java/kono/materialreplication/MRUtils.java
+++ src/main/java/kono/materialreplication/MRUtils.java
@@ -1,6 +1,8 @@
 package kono.materialreplication;
 
+import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 
 public class MRUtils {
 
@@ -9,5 +11,17 @@ public class MRUtils {
 
     public static ResourceLocation mrId(String name) {
         return new ResourceLocation(MODID, name);
+    }
+
+    public static int getProtons(@NotNull Material material) {
+        return (int) material.getProtons();
+    }
+
+    public static int getNeutrons(@NotNull Material material) {
+        return (int) material.getProtons();
+    }
+
+    public static int getMass(@NotNull Material material) {
+        return (int) material.getMass();
     }
 }
---
+++ src/main/java/kono/materialreplication/MaterialReplicationConfig.java
@@ -0,0 +1,50 @@
+package kono.materialreplication;
+
+import com.electronwill.nightconfig.core.ConfigFormat;
+import dev.toma.configuration.Configuration;
+import dev.toma.configuration.config.Config;
+import dev.toma.configuration.config.Configurable;
+import dev.toma.configuration.config.format.ConfigFormats;
+
+@Config(id = MRUtils.MODID)
+public class MaterialReplicationConfig {
+
+    public static MaterialReplicationConfig INSTANCE;
+
+    public static void init() {
+        INSTANCE = Configuration.registerConfig(MaterialReplicationConfig.class, ConfigFormats.yaml()).getConfigInstance();
+    }
+
+    @Configurable
+    public RecipeConfigs recipe = new RecipeConfigs();
+    @Configurable
+    public DeconstructConfigs deconstruct = new DeconstructConfigs();
+    @Configurable
+    public ReplicateConfigs replicate = new ReplicateConfigs();
+
+    public static class DeconstructConfigs {
+
+        @Configurable
+        @Configurable.Comment({"Deconstruction Base time in tick (int).", "Default : 600 tick (30 Sec.)",
+                "Duration : BaseTime * material's Average mass"})
+        @Configurable.Range(min = 1)
+        public int DeconstructionBaseTime = 600;
+
+        @Configurable
+        @Configurable.Comment({"Deconstruction Base Voltage in Long.", "Default : 30 (LV)"})
+        @Configurable.Range(min = 1)
+        public long DeconstructionVoltage = 30;
+
+        @Configurable
+        @Configurable.Comment({"If true, only Elements can be deconstructed.", "Default: false"})
+        public boolean DeconstructOnlyElements = false;
+    }
+
+    public static class ReplicateConfigs {}
+
+    public static class RecipeConfigs {
+        @Configurable
+        @Configurable.Comment({"Whether to add the UUMatter recipe.", "Default : false" })
+        public boolean addMatterRecipe = false;
+    }
+}
