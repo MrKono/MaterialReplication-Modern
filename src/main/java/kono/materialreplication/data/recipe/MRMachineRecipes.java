d67df187637851db49895e4d1a70aec58dc6e352
ko_no <90126004+MrKono@users.noreply.github.com>
2025-07-26 12:16:20 +0900

    [add] recipe

--- src/main/java/kono/materialreplication/data/recipe/MRMachineRecipes.java
+++ src/main/java/kono/materialreplication/data/recipe/MRMachineRecipes.java
@@ -2,6 +2,11 @@ package kono.materialreplication.data.recipe;
 
 import java.util.function.Consumer;
 
+import com.gregtechceu.gtceu.api.GTValues;
+import com.gregtechceu.gtceu.common.data.GTItems;
+import com.gregtechceu.gtceu.data.recipe.CustomTags;
+import com.gregtechceu.gtceu.data.recipe.VanillaRecipeHelper;
+import com.sun.jna.platform.win32.OaIdl;
 import net.minecraft.data.recipes.FinishedRecipe;
 
 import com.gregtechceu.gtceu.api.data.chemical.material.stack.MaterialEntry;
@@ -52,5 +57,34 @@ public class MRMachineRecipes {
                 .outputItems(CASING_ATOMIC.asStack(ConfigHolder.INSTANCE.recipes.casingsPerCraft))
                 .EUt(16).duration(50)
                 .addMaterialInfo(true).save(provider);
+
+        // Large Deconstructor
+        VanillaRecipeHelper.addShapedRecipe(provider, mrId("large_deconstructor"), MRMachines.LARGE_DECONSTRUCTOR.asStack(),
+                "FCF", "EDE", "FQF",
+                'C', CustomTags.ZPM_CIRCUITS,
+                'D', MRMachines.DECONSTRUCTOR[GTValues.LuV].asStack(),
+                'F', GTItems.FIELD_GENERATOR_LuV.asStack(),
+                'E', GTItems.EMITTER_LuV.asStack(),
+                'Q', new MaterialEntry(TagPrefix.cableGtQuadruple, GTMaterials.YttriumBariumCuprate));
+
+        // Large Annihilator
+        VanillaRecipeHelper.addShapedRecipe(provider, mrId("large_annihilator"), MRMachines.LARGE_ANNIHILATOR.asStack(),
+                "PCP", "BSB", "MWM",
+                'P', GTItems.ELECTRIC_PISTON_IV.asStack(),
+                'C', CustomTags.IV_CIRCUITS,
+                'B', GTItems.COMPONENT_GRINDER_TUNGSTEN.asStack(),
+                'S', MRMachines.ANNIHILATOR[GTValues.IV].asStack(),
+                'M', GTItems.ELECTRIC_MOTOR_IV.asStack(),
+                'W', new MaterialEntry(TagPrefix.cableGtDouble, GTMaterials.Platinum));
+
+        // Large Replicator
+        VanillaRecipeHelper.addShapedRecipe(provider, mrId("large_replicator"), MRMachines.LARGE_REPLICATOR.asStack(),
+                "EDE", "CMC", "SPS",
+                'E', GTItems.EMITTER_UV.asStack(),
+                'D', new MaterialEntry(TagPrefix.block, GTMaterials.Neutronium),
+                'C', CustomTags.UHV_CIRCUITS,
+                'M', MRMachines.REPLICATOR[GTValues.UV].asStack(),
+                'S', GTItems.SENSOR_UV.asStack(),
+                'P', new MaterialEntry(TagPrefix.pipeHugeFluid, GTMaterials.Neutronium));
     }
 }
